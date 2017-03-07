014dedd7b4681ed1c1997e7e95295408d5bb0cd2
DoSun <chenxuan958864951@qq.com>
2017-03-08 02:45:56 +0800

    完善排行榜分类下载

--- pixivpy.py
+++ pixivpy.py
@@ -4,9 +4,9 @@ import requests
 import re
 import os
 import datetime
-from bs4 import BeautifulSoup, SoupStrainer
+# from bs4 import BeautifulSoup, SoupStrainer
 # 排行榜默认下载文件数
-RANKING_NUM = 20
+RANKING_NUM = 5
 
 
 # -----------------------------------------------
@@ -60,8 +60,8 @@ def login():
         }
         # 获取post_key
         re1 = s.get(url=login_url1, headers=headers)
-        soup = BeautifulSoup(re1.content, 'lxml', parse_only=SoupStrainer(attrs={'name': 'post_key'}))
-        post_key = soup.find('input')['value']
+        post_key_pattern = re.compile(r'(?<=name="post_key"\svalue=")\S*(?=">)')
+        post_key = re.findall(post_key_pattern, re1.text)[0]
         # 构造data
         data = {
             'pixiv_id': pixiv_id,
@@ -87,37 +87,45 @@ def login():
         return cookies
 
 
-# 取得排行榜信息并下载
-def ranking(num=RANKING_NUM, mode='daily', multi=False):
+# 取得排行榜信息
+def ranking(num, mode, cookies):
     url = 'http://www.pixiv.net/ranking.php?mode=' + mode
-    req = requests.get(url)
+    req = requests.get(url, cookies=cookies)
     assert req.status_code == 200, '链接异常'
     # 这个正则匹配hhhh
     ids_pattern = re.compile(r'(?<="\sdata-id=")\d+(?=">)')
     data_ids = re.findall(ids_pattern, req.text)
-    save_ranking_img(data_ids[:num], mode, multi)
-    return
+    return data_ids[:num]
 
 
 # 下载排行榜图片
-def save_ranking_img(ids, mode, multi):
+def save_ranking_img(ids, mode, multi, cookies):
     referer = 'http://www.pixiv.net/ranking.php?mode=' + mode
     headers = {
         'Referer': referer,
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/56.0.2924.87 Safari/537.36 '
     }
-    # 登录
-    my_cookies = login()
     # 创建一个会话对象
     s = requests.Session()
     s.headers = headers
-    s.cookies = requests.utils.cookiejar_from_dict(my_cookies)
-    # 获取当天日期
-    today = datetime.datetime.now().today().strftime("%Y-%m-%d")
+    s.cookies = requests.utils.cookiejar_from_dict(cookies)
+    # 获取日期
+    day_pattern = re.compile(r'(daily|male|female)(_r18)?')
+    week_pattern = re.compile(r'weekly(_r18)?|rookie|original')
+    today = datetime.datetime.now().today()
+    week = today - datetime.timedelta(weeks=1)
+    month = today - datetime.timedelta(days=30)
+    if re.search(day_pattern, mode):
+        date_name = today.strftime("%Y-%m-%d")
+    elif re.search(week_pattern, mode):
+        date_name = week.strftime("%Y-%m-%d") + '~' + today.strftime("%Y-%m-%d")
+    else:
+        date_name = month.strftime("%Y-%m-%d") + '~' + today.strftime("%Y-%m-%d")
 
     printColor('开始爬取id,请稍后...', 'LIGHTBLUE')
     originals = []
+    multi_num = 0
     for item_id in ids:
         item_url = 'http://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + item_id
         req = s.get(item_url)
@@ -125,6 +133,7 @@ def save_ranking_img(ids, mode, multi):
         original_url = re.findall(original_pattern, req.text)
         # 如果是多P作品的话
         if not original_url:
+            multi_num += 1
             if not multi:
                 originals.append('')
                 continue
@@ -144,69 +153,83 @@ def save_ranking_img(ids, mode, multi):
             originals.append(original_url)
         else:
             originals.append(original_url[0])
-    printColor('id爬取完毕', 'LIGHTBLUE')
+    printColor('id爬取完毕 其中'+str(multi_num)+'个多P作品', 'LIGHTBLUE')
     printColor('开始下载图片', 'LIGHTBLUE')
-    download_path = os.path.join('dailyimg', today)
-    if mode == 'daily':
-        # 创建对应日期的目录
-        if not os.path.exists(download_path):
-            os.makedirs(download_path)
-            printColor(today+'文件夹创建成功', 'LIGHTBLUE')
-        else:
-            printColor(today+'文件夹已存在', 'YELLOW')
-        for original in originals:
-            index = originals.index(original)
-            if original == '':
-                continue
-            if isinstance(original, list):
-                list_name = os.path.join(download_path, '#'+str(index+1)+'_id='+ids[index])
-                # 创建多P作品的文件夹
-                if not os.path.exists(list_name):
-                    os.mkdir(list_name)
-                i = 1
-                for p in original:
-                    pic = s.get(p)
-                    format_pattern = re.compile(r'\.png')
-                    if re.search(format_pattern, p):
-                        img_format = '.png'
-                    else:
-                        img_format = '.jpg'
-                    name = os.path.join(list_name, str(i)+img_format)
-                    with open(name, 'wb') as f:
-                        f.write(pic.content)
-                    i += 1
-            else:
-                pic = s.get(original)
+    mode_path = mode + '_img'
+    download_path = os.path.join(mode_path, date_name)
+
+    # 创建对应日期的目录
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+        printColor(date_name+'文件夹创建成功', 'LIGHTBLUE')
+    else:
+        printColor(date_name+'文件夹已存在', 'YELLOW')
+    for original in originals:
+        index = originals.index(original)
+        if original == '':
+            continue
+        if isinstance(original, list):
+            list_name = os.path.join(download_path, '#'+str(index+1)+'_id='+ids[index])
+            # 创建多P作品的文件夹
+            if not os.path.exists(list_name):
+                os.mkdir(list_name)
+            i = 1
+            for p in original:
+                pic = s.get(p)
                 format_pattern = re.compile(r'\.png')
-                if re.search(format_pattern, original):
+                if re.search(format_pattern, p):
                     img_format = '.png'
                 else:
                     img_format = '.jpg'
-                name = os.path.join(download_path, '#'+str(index+1)+'_id='+ids[index]+img_format)
+                name = os.path.join(list_name, str(i)+img_format)
                 with open(name, 'wb') as f:
                     f.write(pic.content)
-            printColor(ids[index]+' done', 'GREEN')
+                i += 1
+        else:
+            pic = s.get(original)
+            format_pattern = re.compile(r'\.png')
+            if re.search(format_pattern, original):
+                img_format = '.png'
+            else:
+                img_format = '.jpg'
+            name = os.path.join(download_path, '#'+str(index+1)+'_id='+ids[index]+img_format)
+            with open(name, 'wb') as f:
+                f.write(pic.content)
+        printColor(ids[index]+' done', 'GREEN')
 
 
 if __name__ == '__main__':
     intro = '''
-================================================
+=========================================================
  ____ _____  _______     ________   __
 |  _ \_ _\ \/ /_ _\ \   / /  _ \ \ / /
 | |_) | | \  / | | \ \ / /| |_) \ V /
 |  __/| | /  \ | |  \ V / |  __/ | |
-|_|  |___/_/\_\___|  \_/  |_|    |_|    by:DoSun
-================================================
-模式选择:
-1. 下载每日排行榜
+|_|  |___/_/\_\___|  \_/  |_|    |_|
+                                          by:DoSun
+=========================================================
+模式选择:(无输入则退出)
+daily(-r18)  --每日排行榜     weekly(_r18) --每周排行榜
+monthly      --每月排行榜     rookie       --新人排行榜
+male(_r18)   --男性向排行榜   female(_r18) --女性向排行榜
+original     --原创排行榜
     '''
     printColor(intro, 'PURPLE', 1)
-    mode = input('输入选择: ')
-    if mode == '1':
-        num = input('下载前几名[默认前20]: ')
+    # 登录取得cookies
+    my_cookies = login()
+    while True:
+        mode = input('输入选择: ')
+        # 无输入就退出
+        if mode == '':
+            break
+        num = input('下载前几名[默认前5]: ')
         multi_mode = input('下载多P作品吗[y/n]: ')
         if multi_mode == 'Y' or multi_mode == 'y':
             multi = True
         else:
             multi = False
-        ranking(num=int(num), mode='daily', multi=multi)
+        if num == '':
+            num = RANKING_NUM
+        num = int(num)
+        ids = ranking(num, mode, my_cookies)
+        save_ranking_img(ids, mode, multi, my_cookies)
